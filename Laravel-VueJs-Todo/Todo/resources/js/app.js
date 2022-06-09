c29fe6b162bc7239b31bac67c9ab56b968a6f7d8
eglimiraka22 <90408110+eglimiraka22@users.noreply.github.com>
2022-06-09 14:04:51 +0200

    WebDesing Course Finished

---
+++ Laravel-VueJs-Todo/Todo/resources/js/app.js
@@ -0,0 +1,17 @@
+
+require("./bootstrap");
+
+window.Vue = require("vue");
+import Form from "./Form";
+window.Form = Form;
+
+Vue.component(
+    "todo-component",
+    require("./components/TodoComponent.vue").default
+);
+
+
+
+const app = new Vue({ 
+    el: "#app",
+});
---
+++ Laravel-VueJs-Todo/Todo/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+<div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-white display-4">Vue Js Todo APP</h1>
+    <todo-component></todo-component>
+    <br><br><br><br>
+    <h2>Klaudio Miraka</h2>
+
+</div>
+
+<style>
+
+
+</style>
+@endsection
